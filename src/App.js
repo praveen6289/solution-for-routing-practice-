37f90df9458fb8fbe35a1a541e9d2960e92d780d
praveen6289 <kuthadipraveen366@gmail.comgit config --global user.name praveen6289>
2022-07-09 12:21:21 +0000

    Solution for routing practise app

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import {Route, Switch} from 'react-router-dom'
+import NotFound from './components/NotFound'
+
+import Header from './components/Header'
+import Home from './components/Home'
+import About from './components/About'
+import Contact from './components/Contact'
+
+import './App.css'
+
+const App = () => (
+  <div className="app-bg-container">
+    <div className="app-content-container">
+      <Header />
+      <div className="app-body">
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/about" component={About} />
+          <Route exact path="/contact" component={Contact} />
+          <Route component={NotFound} />
+        </Switch>
+      </div>
+    </div>
+  </div>
+)
+
+export default App
